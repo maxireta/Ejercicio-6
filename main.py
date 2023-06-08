a73fd2c11af61d10743e778b313ed8cdcd1f309b
maxireta <maxi_reta@hotmail.com>
2023-06-07 21:08:06 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from claseColeccion import coleccion
+
+if __name__ == '__main__':
+    cole = coleccion()
+    cole.cargar()
+    print("Menú:")
+    print("1. Insertar un vehículo.")
+    print("2. Agregar un vehículo.")
+    print("3. Mostrar tipo elemento.")
+    print("4. Modificar precio por patente.")
+    print("5. Mostrar todos los datos.")
+    print("6. Mostrar vehículos a la venta.")
+    print("7. Almacenar datos.")
+    opcion = input("Elija una opción: ")
+    if opcion == '1':
+        pos = int(input("Ingrese la posición: "))
+        pos -= 1
+        auto = cole.crearvehiculo()
+        cole.insertarElemento(auto, pos)
+    elif opcion == '2':
+        auto = cole.crearvehiculo()
+        cole.agregarElemento(auto)
+    elif opcion == '3':
+        posicion = int(input("Ingrese una posición: "))
+        posicion -= 1
+        cole.mostrar3(posicion)
+    elif opcion == '4':
+        pate = input("Ingrese la patente: ")
+        cole.opcion4(pate)
+    elif opcion == '5':
+        cole.opcion5()
+    elif opcion == '6':
+        cole.opcion6()
+    elif opcion == '7':
+        cole.opcion7()
